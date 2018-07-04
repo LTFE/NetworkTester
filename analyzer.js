312921b5ffcf008d35aded0448c7875737da69ee
dolencd <dolencd>
2018-07-04 17:13:17 +0200

    fixed connection error

--- analyzer.js
+++ analyzer.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const Web3 = require("web3");
 const logger = require('./csvLogger')('finalResults','\t');
-const web3 = new Web3(new Web3.providers.HttpProvider("s"));
+const web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
 const inputFileName = process.argv[2];
 
 
